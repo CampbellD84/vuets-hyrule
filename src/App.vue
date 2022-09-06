ad21d553e81bf0abc831c4f54190ff8a16a9813d
Douglas Campbell <dacampbelltbone@gmail.com>
2022-09-06 00:20:40 -0400

    complete tutorial of Vue 3 w/ TypeScript Hyrule Jobs

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,105 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <header>
+      <div class="title">
+        <img src="./assets/heart.svg" alt="site logo" />
+        <h1>Hyrule Jobs</h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">Order by Title</button>
+        <button @click="handleClick('salary')">Order by Salary</button>
+        <button @click="handleClick('location')">Order by Location</button>
+      </div>
+    </header>
+    <JobsList :jobs="jobs" :order="order" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, ref } from "vue";
+import JobsList from "./components/JobsList.vue";
+import Job from "./types/Job";
+import OrderTerm from "./types/OrderTerm";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: { JobsList },
+  setup() {
+    const jobs = ref<Job[]>([
+      {
+        title: "Farm Worker",
+        location: "Lon Lon Ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "Quarryman",
+        location: "Death Mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "Flute Player",
+        location: "The Lost Woods",
+        salary: 35000,
+        id: "3",
+      },
+      {
+        title: "Fisherman",
+        location: "Lake Hylia",
+        salary: 21000,
+        id: "4",
+      },
+      {
+        title: "Prison Guard",
+        location: "Gerudo Valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
+
+    const order = ref<OrderTerm>("title");
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term;
+    };
+
+    return { jobs, handleClick, order };
+  },
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+header {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+header .order {
+  margin-top: 20px;
+}
+
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+
+header .title {
+  display: flex;
+  justify-content: center;
+}
+
+header img {
+  width: 60px;
+  margin-right: 20px;
+}
+
+header h1 {
+  font-size: 3em;
 }
 </style>
